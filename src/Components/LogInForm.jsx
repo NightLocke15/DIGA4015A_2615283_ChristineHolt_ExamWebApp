620688549b3b06123a01eec14fa76bd68770e2d4
NightLocke15 <christinefrances.holt@gmail.com>
2025-05-06 16:03:05 +0200

    Information Structure Part 1 | Folder and File Creation (partly)

---
+++ src/Components/LogInForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogInForm() {
+
+}
+
+export default LogInForm;
---
+++ src/Pages/CreateCharacter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateCharacter() {
+
+}
+
+export default CreateCharacter;
