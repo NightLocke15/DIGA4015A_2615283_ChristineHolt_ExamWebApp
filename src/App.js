667d1488567ab8859d18ab5efb2b7831f7022aab
NightLocke15 <christinefrances.holt@gmail.com>
2025-05-08 20:14:41 +0200

    Create Account Page Part 1 | Started adding functionality for  creating a new account

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Styles/Misc.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateProfile from './Pages/CreateProfile';
+import UserProvider from './Context/UserContext';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      
-      </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <CreateProfile />
+        </BrowserRouter>
+      </UserProvider>
     </div>
   );
 }
--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -2,6 +2,21 @@ import React from "react";
 
 function LogIn() {
 
+    return (
+        <React.Fragment>
+            <h1>LOG IN</h1>
+            <form>
+                <label>
+                    UserName:
+                    <input type="text" />
+                </label>
+                <label>
+                    Password:
+                    <input type="text" />
+                </label>
+            </form>            
+        </React.Fragment>
+    )
 }
 
 export default LogIn;
